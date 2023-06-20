4fa3eadfa6d8d41367493a0349c111fc673295e8
Joao Batista Lopes Filho <batistafilho.lopes@gmail.com>
2023-06-20 23:32:38 +0200

    JavaScript Pratica

---
+++ main.js
@@ -0,0 +1,11 @@
+function tocaSomPom() {
+    document.querySelector("#som_tocla_pom").play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+while () {
+    listaDeTeclas[0].onclick = tocaSomPom;
+}
